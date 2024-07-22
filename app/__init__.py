8671af6dafbcbd2ac1b0da649714e5c9ac1d3f54
Yuriy Semenov <info@juriys.ru>
2024-07-22 21:31:59 +0300

    add base.html

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,7 @@ app = Flask(__name__)  # type: Flask
 # Нижний импорт - это хорошо известное решение,
 # позволяющее избежать циклического импорта, распространенной проблемы с приложениями Flask.
 from app import routes  # type: ignore
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 """
 Маршрут на главную страницу
 """
+from flask import render_template
+
 from app import app
-@app.route("/") # главная страница - / (без пути)
-@app.route("/index") # главная страница - /index (по умолчанию)
-def index(): return "Hello World! - это главная страница"
+@app.route("/")  # главная страница - / (без пути)
+@app.route("/index")  # главная страница - /index (по умолчанию)
+def index():
+    user = {'username': 'Yuriy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template(
+        'index.html', title='MainPage', user=user, posts=posts
+                           )
