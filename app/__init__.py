4060f9b971e35d9ec085bf89d5ea98b186425ab5
Yuriy Semenov <info@juriys.ru>
2024-08-18 17:25:32 +0300

    add migrations user

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,19 @@
 возникающей в результате взаимных ссылок между этими двумя файлами.
 """
 
-from flask import Flask
-
 from config import Config
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)  # type: Flask
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # Нижний импорт - это хорошо известное решение,
 # позволяющее избежать циклического импорта, распространенной проблемы с приложениями Flask.
-from app import routes  # type: ignore
+from app import routes, models  # type: ignore
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -12,4 +12,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
-git
