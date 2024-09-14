664616798730a853665bfc9ec298459906238374
Yuriy Semenov <info@juriys.ru>
2024-09-14 19:54:53 +0300

    add autorisation

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,10 @@
 from flask import flash, redirect, render_template, url_for
 from app import app
 from app.forms import LoginForm
-
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route("/")  # главная страница - / (без пути)
 @app.route("/index")  # главная страница - /index (по умолчанию)
@@ -22,26 +25,22 @@ def index():
     ]
     return render_template("index.html", title="MainPage", user=user, posts=posts)
 
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    """
-    This function handles the login route and renders the login.html template.
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = db.session.scalar(
+      sa.select(User).where(User.username == form.username.data))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    return redirect(url_for('index'))
+  return render_template('login.html', title='Sign In', form=form)
 
-    It creates an instance of the LoginForm class and passes it to the login.html template.
-    The template is rendered with the title "Sign In".
-
-    Returns:
-        The rendered login.html template.
-    """
-    # Create an instance of the LoginForm class
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(
-            "Login requested for user {}, remember_me={}".format(
-                form.username.data, form.remember_me.data
-            )
-        )
-        return redirect(url_for("index"))
-    # Render the login.html template with the form and title
-    return render_template("login.html", title="Sign In", form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
