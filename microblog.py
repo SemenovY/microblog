a762b9fc39ca0016031db81cee7b0b571baf37cb
Yuriy Semenov <info@juriys.ru>
2024-10-12 16:36:43 +0300

    add app and config

--- microblog.py
+++ microblog.py
@@ -1,16 +1,12 @@
-"""
-Точка входа в блог
-Для завершения работы приложения у вас должен быть код на Python верхнего уровня,
-который определяет экземпляр приложения Flask.
-"""
-
-from app import app
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db
-from app.models import User, Post
+from app import create_app, db
+from app.models import User, Post, Message, Notification, Task
+
+app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-  return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post,
+            'Message': Message, 'Notification': Notification, 'Task': Task}
--- migrations/versions/e517276bb1c2_users_table.py
+++ migrations/versions/e517276bb1c2_users_table.py
@@ -1,8 +1,8 @@
 """users table
 
-Revision ID: e5a6409bb88d
+Revision ID: e517276bb1c2
 Revises: 
-Create Date: 2024-08-18 17:20:10.519776
+Create Date: 2017-09-11 11:23:05.566844
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e5a6409bb88d'
+revision = 'e517276bb1c2'
 down_revision = None
 branch_labels = None
 depends_on = None
